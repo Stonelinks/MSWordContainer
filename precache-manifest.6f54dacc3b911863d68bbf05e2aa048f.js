0f81b2211b24d9f182fbf22c8eecb4e631809731
Lucas Doyle <lucas.p.doyle@gmail.com>
2018-11-16 16:18:32 -0500

    Updates

---
+++ precache-manifest.6f54dacc3b911863d68bbf05e2aa048f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5a923c1417782550a789",
+    "url": "/static/js/main.5a923c14.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "5a923c1417782550a789",
+    "url": "/static/css/main.bfea5086.chunk.css"
+  },
+  {
+    "revision": "82286f363fe31851ff29c01450b969fa",
+    "url": "/index.html"
+  }
+];
